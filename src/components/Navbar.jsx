b07c26c77801b06ecc3679a55ce6a120ce11d417
ifeoluwa oladeni <oladeniifeoluwa123@gmail.com>
2024-02-04 21:33:11 +0100

    Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,315 @@
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import logo from '../assests/logo.png';
+import styles from './Navbar.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faBars,
+  faChevronDown,
+  faChevronUp,
+  faXmark,
+} from '@fortawesome/free-solid-svg-icons';
+
+const Navbar = () => {
+  const [resourcesDisplay, setResourcesDisplay] = useState(false);
+  const [eventDisplay, setEventDisplay] = useState(false);
+  const [aboutDisplay, setAboutDisplay] = useState(false);
+  const [navMenu, setNavMenu] = useState(false);
+  const toggleNavbar = () => {
+    setNavMenu(!navMenu);
+  };
+  const toggleResources = () => {
+    setResourcesDisplay(!resourcesDisplay);
+  };
+  const toggleEvent = () => {
+    setEventDisplay(!eventDisplay);
+  };
+  const toggleAbout = () => {
+    setAboutDisplay(!aboutDisplay);
+  };
+  return (
+    <>
+      <div className={styles['navbar-container']}>
+        <NavLink to='/'>
+          <div className={styles['logo-container']}>
+            <img src={logo} alt='logo' />
+          </div>
+        </NavLink>
+
+        <ul className={styles['nav-links']}>
+          <li className={styles['nav-link']}>
+            {' '}
+            <NavLink to='/'>Home</NavLink>{' '}
+          </li>
+          <li onClick={toggleAbout} className={styles['nav-link']}>
+            {' '}
+            <NavLink
+              to='/about'
+              className={
+                aboutDisplay === true ? `${styles['project-active']}` : ''
+              }
+            >
+              About
+            </NavLink>
+            {aboutDisplay ? (
+              <FontAwesomeIcon
+                icon={faChevronUp}
+                style={{ color: '#106fb8' }}
+              />
+            ) : (
+              <FontAwesomeIcon
+                icon={faChevronDown}
+                style={{ color: '#74c0fc' }}
+              />
+            )}
+          </li>
+          <li onClick={toggleEvent} className={styles['nav-link']}>
+            {' '}
+            <NavLink
+              to='/events'
+              className={
+                eventDisplay === true ? `${styles['project-active']}` : ''
+              }
+            >
+              Events
+            </NavLink>
+            {eventDisplay ? (
+              <FontAwesomeIcon
+                icon={faChevronUp}
+                style={{ color: '#106fb8' }}
+              />
+            ) : (
+              <FontAwesomeIcon
+                icon={faChevronDown}
+                style={{ color: '#74c0fc' }}
+              />
+            )}
+          </li>
+          <li onClick={toggleResources} className={styles['nav-link']}>
+            {' '}
+            <NavLink
+              to='/resources'
+              className={
+                resourcesDisplay === true ? `${styles['project-active']}` : ''
+              }
+            >
+              Resources
+            </NavLink>
+            {resourcesDisplay ? (
+              <FontAwesomeIcon
+                icon={faChevronUp}
+                style={{ color: '#106fb8' }}
+              />
+            ) : (
+              <FontAwesomeIcon
+                icon={faChevronDown}
+                style={{ color: '#74c0fc' }}
+              />
+            )}
+          </li>
+          <li className={styles['nav-link']}>
+            {' '}
+            <NavLink to='giving'>Giving</NavLink>{' '}
+          </li>
+
+          <li className={styles['nav-link']}>
+            {' '}
+            <NavLink to='/contact'> Contact</NavLink>
+          </li>
+        </ul>
+
+        <div className={styles['hamburger-btn']}>
+          {navMenu ? (
+            <div className={styles['toggle-btn']} onClick={toggleNavbar}>
+              <FontAwesomeIcon icon={faXmark} style={{ color: '#74C0FC' }} />
+            </div>
+          ) : (
+            <div className={styles['toggle-btn']} onClick={toggleNavbar}>
+              <FontAwesomeIcon icon={faBars} style={{ color: '#74C0FC' }} />
+            </div>
+          )}{' '}
+        </div>
+      </div>
+      {eventDisplay && (
+        <div className={styles['projects-container']}>
+          <ul>
+            <li className={styles['project-list']}>
+              {' '}
+              <NavLink to='/projects/solar-borehole'>
+                Solar Boreholes
+              </NavLink>{' '}
+            </li>
+            <li className={styles['project-list']}>
+              {' '}
+              <NavLink to='/projects/empowerment-project'>
+                Empowerment Projects
+              </NavLink>{' '}
+            </li>
+          </ul>
+        </div>
+      )}
+
+      {navMenu && (
+        <div className={styles['navbar-dropDown']}>
+          <ul className={styles['mobile-nav-links']}>
+            <li className={styles['nav-link']}>
+              {' '}
+              <NavLink to='/'>Home</NavLink>{' '}
+            </li>
+            <li onClick={toggleAbout} className={styles['nav-link']}>
+              {' '}
+              <NavLink
+                to='/about'
+                className={
+                  aboutDisplay === true ? `${styles['project-active']}` : ''
+                }
+              >
+                About
+              </NavLink>
+              {aboutDisplay ? (
+                <FontAwesomeIcon
+                  icon={faChevronUp}
+                  style={{ color: '#106fb8' }}
+                />
+              ) : (
+                <FontAwesomeIcon
+                  icon={faChevronDown}
+                  style={{ color: '#74c0fc' }}
+                />
+              )}
+            </li>
+            {aboutDisplay && (
+              <li className={styles['nav-sub-link']}>
+                {' '}
+                <NavLink to=''>lorem</NavLink>{' '}
+              </li>
+            )}
+            {aboutDisplay && (
+              <li className={styles['nav-sub-link']}>
+                {' '}
+                <NavLink to=''>ipsum</NavLink>{' '}
+              </li>
+            )}
+            <li onClick={toggleEvent} className={styles['nav-link']}>
+              {' '}
+              <NavLink
+                to='/events'
+                className={
+                  eventDisplay === true ? `${styles['project-active']}` : ''
+                }
+              >
+                Events
+              </NavLink>
+              {eventDisplay ? (
+                <FontAwesomeIcon
+                  icon={faChevronUp}
+                  style={{ color: '#106fb8' }}
+                />
+              ) : (
+                <FontAwesomeIcon
+                  icon={faChevronDown}
+                  style={{ color: '#74c0fc' }}
+                />
+              )}
+            </li>
+            {eventDisplay && (
+              <li className={styles['nav-sub-link']}>
+                {' '}
+                <NavLink to=''>lorem</NavLink>{' '}
+              </li>
+            )}
+            {eventDisplay && (
+              <li className={styles['nav-sub-link']}>
+                {' '}
+                <NavLink to=''>ipsum</NavLink>{' '}
+              </li>
+            )}
+            <li onClick={toggleResources} className={styles['nav-link']}>
+              {' '}
+              <NavLink
+                to='/resources'
+                className={
+                  resourcesDisplay === true ? `${styles['project-active']}` : ''
+                }
+              >
+                Resources
+              </NavLink>
+              {resourcesDisplay ? (
+                <FontAwesomeIcon
+                  icon={faChevronUp}
+                  style={{ color: '#106fb8' }}
+                />
+              ) : (
+                <FontAwesomeIcon
+                  icon={faChevronDown}
+                  style={{ color: '#74c0fc' }}
+                />
+              )}
+            </li>
+            {resourcesDisplay && (
+              <li className={styles['nav-sub-link']}>
+                {' '}
+                <NavLink to=''>Lorem</NavLink>{' '}
+              </li>
+            )}
+            {resourcesDisplay && (
+              <li className={styles['nav-sub-link']}>
+                {' '}
+                <NavLink to=''>ipsum</NavLink>{' '}
+              </li>
+            )}
+            <li className={styles['nav-link']}>
+              {' '}
+              <NavLink to='giving'>Giving</NavLink>{' '}
+            </li>
+
+            <li className={styles['nav-link']}>
+              {' '}
+              <NavLink to='/contact'> Contact</NavLink>
+            </li>
+          </ul>
+          {/* <ul className={styles["mobile-nav-links"]}>
+            <li className={styles["nav-link"]}>
+              {" "}
+              <NavLink to="/about-us"> About Us</NavLink>{" "}
+            </li>
+            <li className={styles["nav-link"]}>
+              {" "}
+              <NavLink to="/volunteer">Volunteer</NavLink>{" "}
+            </li>
+            <li onClick={toggleProject} className={styles["nav-link"]}>
+              {" "}
+              <span
+                className={
+                  projectDisplay === true ? `${styles["project-active"]}` : ""
+                }
+              >
+                Projects
+              </span>
+              {projectDisplay ? (
+                <img src="/dropup_btn.svg" alt="dropdown_icon" />
+              ) : (
+                <img src="/dropdown_btn.svg" alt="dropdown_icon" />
+              )}
+            </li>
+            
+            <li className={styles["nav-link"]}>
+              {" "}
+              <NavLink to="/contact-us">Contact Us</NavLink>{" "}
+            </li>
+            <li className={styles["nav-link"]}>
+              {" "}
+              <NavLink to="/events">Events</NavLink>{" "}
+            </li>
+            <li className={styles["nav-link"]}>
+              {" "}
+              <NavLink to="/blog-posts">Blog</NavLink>{" "}
+            </li>
+          </ul> */}
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Navbar;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,52 +1,54 @@
-import React from 'react';
-import Carousel from '../components/Carousel';
-import ReadMore from '../components/ReadMore';
-import Pastor from '../assests/Pastor.jpeg';
-import Footer from '../components/Footer';
+import React from "react";
+import Carousel from "../components/Carousel";
+import ReadMore from "../components/ReadMore";
+import Pastor from "../assests/Pastor.jpeg";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
 
 const Home = () => {
   const loremIpsum =
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam non tortor vitae eros tristique euismod. Sed eget odio vel dolor fringilla efficitur.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam non tLorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam non tLorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam non tLorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam non tLorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam non t';
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam non tortor vitae eros tristique euismod. Sed eget odio vel dolor fringilla efficitur.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam non tLorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam non tLorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam non tLorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam non tLorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam non t";
 
   const pastor =
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam non tortor vitae eros tristique euismod. Sed eget odio vel dolor fringilla efficitur.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam non tLorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam non tLorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam non tLorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam non tLorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam non t';
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam non tortor vitae eros tristique euismod. Sed eget odio vel dolor fringilla efficitur.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam non tLorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam non tLorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam non tLorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam non tLorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam non t";
 
   return (
     <div>
+      <Navbar />
       <Carousel />
       <div>
-        <h1 className='welcome'>WELCOME TO</h1>
-        <p className='winners'>Winners Baptist Church, Bariga</p>
+        <h1 className="welcome">WELCOME TO</h1>
+        <p className="winners">Winners Baptist Church, Bariga</p>
       </div>
       <div>
         <ReadMore text={loremIpsum} maxLength={150} />
       </div>
 
       {/* worship */}
-      <div className=' bg-light p-28 mt-14'>
-        <div className='text-center mb-4'>
-          <h1 className='text-4xl font-bold text-blue'>Worship Avenue</h1>
-          <p className='text-gray-600'>
+      <div className=" bg-light p-28 mt-14">
+        <div className="text-center mb-4">
+          <h1 className="text-4xl font-bold text-blue">Worship Avenue</h1>
+          <p className="text-gray-600">
             Fellowship with us on any of these days
           </p>
-          <div className='flex flex-col items-center md:flex-row md:justify-around gap-4 pt-8'>
-            <div className='flex-item bg-white px-16  py-5 rounded-md text-center'>
-              <div className='square mb-2 flex flex-row items-center'>
-                <p className='text-white bg-blue px-7 py-7 rounded-md font-semibold'>
+          <div className="flex flex-col items-center md:flex-row md:justify-around gap-4 pt-8">
+            <div className="flex-item bg-white px-16  py-5 rounded-md text-center">
+              <div className="square mb-2 flex flex-row items-center">
+                <p className="text-white bg-blue px-7 py-7 rounded-md font-semibold">
                   Sun
                 </p>
-                <p className='text-black ml-8 font-semibold'>
-                  Combined service <span className=' font-medium'>8:00am</span>
+                <p className="text-black ml-8 font-semibold">
+                  Combined service <span className=" font-medium">8:00am</span>
                 </p>
               </div>
             </div>
-            <div className='flex-item bg-white px-16  py-5 rounded-md text-center'>
-              <div className='square mb-2 flex flex-row items-center'>
-                <p className='text-white bg-green px-7 py-7 rounded-md font-semibold'>
+            <div className="flex-item bg-white px-16  py-5 rounded-md text-center">
+              <div className="square mb-2 flex flex-row items-center">
+                <p className="text-white bg-green px-7 py-7 rounded-md font-semibold">
                   Wed
                 </p>
-                <p className='text-black ml-8 font-semibold'>
-                  Victory Hour <span className=' font-medium'>6:00pm</span>
+                <p className="text-black ml-8 font-semibold">
+                  Victory Hour <span className=" font-medium">6:00pm</span>
                 </p>
               </div>
             </div>
@@ -55,13 +57,13 @@ const Home = () => {
       </div>
 
       {/* message from our pastor */}
-      <div className='flex flex-col items-center px-6 md:flex-row md:justify-around gap-4 pt-8 px-32'>
-        <div className='flex flex-col  md:flex-row md: justify-evenly gap-4'>
-          <div className='flex-item bg-blue-500 p-4 rounded-md flex-1'>
-            <h2 className='text-3xl font-medium mb-2 text-blue'>
+      <div className="flex flex-col items-center px-6 md:flex-row md:justify-around gap-4 pt-8 px-32">
+        <div className="flex flex-col  md:flex-row md: justify-evenly gap-4">
+          <div className="flex-item bg-blue-500 p-4 rounded-md flex-1">
+            <h2 className="text-3xl font-medium mb-2 text-blue">
               Message from Our Pastor
             </h2>
-            <p className='text-gray-700'>
+            <p className="text-gray-700">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
               non tortor vitae eros tristique euismod. Sed eget odio vel dolor
               fringilla efficitur.Lorem ipsum dolor sit amet, consectetur
@@ -80,15 +82,15 @@ const Home = () => {
               dolor sit amet, consectetur adipiscing elit. Nullam non t
             </p>
           </div>
-          <div className=' flex-1 md:flex-item p-10 rounded-md text-center flex-1/2'>
+          <div className=" flex-1 md:flex-item p-10 rounded-md text-center flex-1/2">
             <img
               src={Pastor}
-              alt='pastors image'
-              className='mb-2 pl-18 rounded-md  h-auto '
+              alt="pastors image"
+              className="mb-2 pl-18 rounded-md  h-auto "
             />
-            <p className=' font-semibold'>
+            <p className=" font-semibold">
               Rev Olusola Oladeni, Ph.D <br></br>
-              <span className=' font-medium'>Church Pastor</span>
+              <span className=" font-medium">Church Pastor</span>
             </p>
           </div>
         </div>
